f65a35a02c2d9b803a4ec426cca0fb6d3e26cecc
Bobby Jones Jr <jones9682@yahoo.com>
2020-06-06 12:54:48 -0400

    added some code for user input

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var uppercaseSelection = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercaseSelection = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var specialCharacters = "!@#$^&%*()+=-[]{}|:<>?,.'";
+
+var password = "";
 
 // Write password to the #password input
 function writePassword() {
@@ -7,6 +13,35 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
+
+// Start function to generate password
+var enter;
+var confirmUppercase;
+var confirmLowercase;
+var confirmNumbers;
+var confirmCharacters;
+
+function generatePassword() {
+  //User input
+  enter = parseInt(
+    prompt(
+      "How many characters would you like your password? Choose between 8 and 128"
+    )
+  );
+  //First if statement for user validation
+  if (!enter) {
+    alert("This needs a value");
+  } else if (enter < 8 || enter > 128) {
+    //Start user input prompts
+    enter = parseInt(prompt("You must choose between 8 and 128"));
+  } else {
+    //Continues once user input is validated
+    confirmUppercase = confirm("Will this contain uppercase letters?");
+    confirmLowercase = confirm("Will this contain lowercase leters?");
+    confirmNumbers = confirm("Will this contain numbers?");
+    confirmCharacters = confirm("Will this contain characters?");
+  }
 }
 
 // Add event listener to generate button
