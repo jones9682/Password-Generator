9978b58e5029b036e739d24864ea33d92aa7700e
Bobby Jones Jr <jones9682@yahoo.com>
2020-06-08 06:51:45 -0400

    changed code for user input

--- script.js
+++ script.js
@@ -1,49 +1,23 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-var uppercaseSelection = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowercaseSelection = "abcdefghijklmnopqrstuvwxyz";
-var numbers = "0123456789";
-var specialCharacters = "!@#$^&%*()+=-[]{}|:<>?,.'";
+//define variables
 
-var password = "";
+var lower = 'abcdefghijklmnopqrstuvwxyz';
+var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var special = '!@#$^&%*()+=-[]{}|:<>?,.';
+var numbers = '1234567890';
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var pwd = '';
 
-  passwordText.value = password;
-}
+var lowerSelection = false;
+var upperSelection = false;
+var specialSelection = false;
+var numberSelection = false;
 
-// Start function to generate password
-var enter;
-var confirmUppercase;
-var confirmLowercase;
-var confirmNumbers;
-var confirmCharacters;
-
-function generatePassword() {
-  //User input
-  enter = parseInt(
-    prompt(
-      "How many characters would you like your password? Choose between 8 and 128"
-    )
-  );
-  //First if statement for user validation
-  if (!enter) {
-    alert("This needs a value");
-  } else if (enter < 8 || enter > 128) {
-    //Start user input prompts
-    enter = parseInt(prompt("You must choose between 8 and 128"));
-  } else {
-    //Continues once user input is validated
-    confirmUppercase = confirm("Will this contain uppercase letters?");
-    confirmLowercase = confirm("Will this contain lowercase leters?");
-    confirmNumbers = confirm("Will this contain numbers?");
-    confirmCharacters = confirm("Will this contain characters?");
-  }
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-copyBtn2.addEventListener("click", copyPassword);
+function generate() {
+    var confirmLength = '';
+//asking user to input desired character length
+    while (isNaN(confirmLength) || confirmLength < 8 || confirmLength > 128) {
+        confirmLength = prompt("What length would you like the password to be? (Between 8 to 128 characters)");
+        if (confirmLength === null) {
+            break;
+        }
+    }
