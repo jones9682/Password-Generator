735dda42a51a3d5b1a4f30a5f94cfce7967aff2b
Bobby Jones Jr <jones9682@yahoo.com>
2020-06-08 22:47:15 -0400

    added 4 negative and 4 positive options

--- script.js
+++ script.js
@@ -1,28 +1,62 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numberChar = "0123456789";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
 var specialChar = "!@#$%^&*()_-+={}[];:'`~<,>.?/|";
 
-//Function used to determine the length of the password
-function determineLength() {
-  passwordLength = prompt(
-    "Choose how many characters long you'd like your password to be (between 8-128 characters): "
-  );
+var enter;
+var confirmLowercase;
+var confirmUppercase;
+var confirmNumbers;
+var confirmSpecialChar;
+var choices;
 
-  if (passwordLength < 8) {
-    alert("Password length must be a number between 8-128 characters");
-    determineLength();
-  } else if (passwordLength > 128) {
-    alert("Password length must be a number between 8-128 characters");
-    determineLength();
+var get = document.querySelector("#generate");
+
+get.addEventListener("click", function () {
+  ps = generatePassword();
+  document.getElementById("password").placeholder = ps;
+});
+
+// Function to generate password
+function generatePassword() {
+  enter = parseInt(
+    prompt(
+      "Choose how many characters long you'd like your password to be (between 8-128 characters): "
+    )
+  );
+  if (!enter) {
+    alert("This needs a value");
+  } else if (enter < 8 || enter > 128) {
+    // Validates user input
+    // Start user input prompts
+    enter = parseInt(prompt("You must choose between 8 and 128"));
+  } else {
+    //Continues once user input is validated
+    confirmLowercase = confirm("Will this contain Lowercase letters");
+    confirmUppercase = confirm("Will this contain Uppercase letters");
+    confirmNumbers = confirm("Will this contain Numbers");
+    confirmSpecialChar = confirm("Will this contain Special characters");
+  }
+  // if for 4 negative options
+  if (
+    !confirmLowercase &&
+    !confirmUppercase &&
+    !confirmNumbers &&
+    !confirmSpecialChar
+  ) {
+    choices = alert("You must choose a criteria!");
+    //  Else if 4 positive options
+  } else if (
+    confirmLowercase &&
+    confirmUppercase &&
+    confirmNumbers &&
+    confirmSpecialChar
+  ) {
+    choices = lowercase.concat(uppercase, numbers, specialChar);
   }
 }
-function generatePassword() {
-  determineLength();
-}
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
